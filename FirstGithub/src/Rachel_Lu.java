231de9454d30fe3e0d9c4a31f4fb75b1159b2d73
aedan <113115936+FRHSDaedan@users.noreply.github.com>
2024-02-20 12:21:29 -0500

    Update Rachel_Lu.java

--- FirstGithub/src/Rachel_Lu.java
+++ FirstGithub/src/Rachel_Lu.java
@@ -5,5 +5,8 @@ public class Rachel_Lu
         System.out.println("Hello, I hope this messages meets you in well regards,\nHello Rachel No Middle Name Lu, \nI must depart now, adios.");
         System.out.println("Hello Rachel");
         System.out.println("Hello Hiro");
+        for(int i=0; i < 16; i++){
+            System.out.println("I'm " + i + " years old.");
+        }
     }
 }
